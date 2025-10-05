a8c0e01f6dc00adfb984fbf744aab027a071b879
Baraa El-Mallah <baraa.elmallah@gmail.com>
2025-10-05 12:18:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1156270502-d2f5d",
+  "appId": "1:311591288473:web:3d7eaaf89c56429ebc4208",
+  "apiKey": "AIzaSyA181VowSaLT3HCrr6_INlmBmgoAu5pFj8",
+  "authDomain": "studio-1156270502-d2f5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "311591288473"
+};
